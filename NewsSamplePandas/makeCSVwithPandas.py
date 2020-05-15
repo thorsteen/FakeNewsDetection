400e333f9128f3e06570fbb70b2c9023a02e9ce5
Kaspi2 <kasper.flong@hotmail.com>
2020-05-15 13:32:07 +0200

    stash

--- NewsSamplePandas/makeCSVwithPandas.py
+++ NewsSamplePandas/makeCSVwithPandas.py
@@ -11,13 +11,13 @@ import re
 import pandas as pd
 import time
 import numpy as np
-#import csv
+# import csv
 start_time = time.time()
 
-#filename ='news_sample.csv' #works with this data
-#filename ='../../../../../../data/1mio-raw.csv'
-#filename = '../../Data/clean-100k.csv' #works with this data
-filename = '../../Data/1mio-raw.csv' # does not work yet
+# filename ='news_sample.csv' #works with this data
+# filename ='../../../../../../data/1mio-raw.csv'
+# filename = '../../Data/clean-100k.csv' #works with this data
+filename = '../../Data/1mio-raw.csv'  # does not work yet
 """
 1mio-raw contains difficult empty rows and other problems which may cause folling err:
 
@@ -27,14 +27,15 @@ natural gas, nuclear, Energy Transfer Partners, Doomsday Clock'
 err occured after chunk 11 was done
 """
 
-#following is csv file is without empty rows and made from 1mio-raw
-#filename = '../../Data/500k.csv'
+# following is csv file is without empty rows and made from 1mio-raw
+# filename = '../../Data/500k.csv'
 
-#så man kan se mere print i terminal
+# så man kan se mere print i terminal
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
+
 
 def clean_text(content):
 
@@ -42,7 +43,7 @@ def clean_text(content):
     clean_text = content.lower()
 
     # Clean dates
-    date = r"(?:jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|(nov|dec)(?:ember)?|(?:[\d]{1}|[\d]{2}))(?: |. |, |/|\\|:|;|.|,)(?:[\d]{1}|[\d]{2})(?: |. |, |/|\\|:|;|.|,)(?:1\d{3}|2\d{3})" #does the same as 1,2,3,8 and 9
+    date = r"(?:jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|(nov|dec)(?:ember)?|(?:[\d]{1}|[\d]{2}))(?: |. |, |/|\\|:|;|.|,)(?:[\d]{1}|[\d]{2})(?: |. |, |/|\\|:|;|.|,)(?:1\d{3}|2\d{3})"  #does the same as 1,2,3,8 and 9
     clean_text = re.sub(date, ' <DATE> ', clean_text)
 
     # Clean email
@@ -63,6 +64,7 @@ def clean_text(content):
 
     return clean_text
 
+
 def putinDic(dictionary, liste):
     liste = list(set(liste))
     liste.sort()
@@ -72,15 +74,17 @@ def putinDic(dictionary, liste):
         dictionary[info] = ID
         ID += 1
 
+
 def simpleEntityToCSV(filename, dictionary):
-    file = open(filename,"w+",encoding="utf-8")
+    file = open(filename, "w+", encoding="utf-8")
     for item in dictionary.items():
         if item[1] != '':
-            file.write("%s,%s\n" %(str(item[1]), str(item[0])))
+            file.write("%s,%s\n" % (str(item[1]), str(item[0])))
     file.close
 
-#------------------------------------------#
-#we give datatypes beforehand and relevant cols to pd.read_csv for better performance
+
+# ------------------------------------------#
+# we give datatypes beforehand and relevant cols to pd.read_csv for better performance
 """
 datatypes = {'id': np.int32, 
              'domain': str, 
@@ -98,14 +102,22 @@ datatypes = {'id': np.int32,
              'tags':str
              }
 """
-cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] #to skp index, source and meta_description column
-df_chunk = pd.read_csv(filename, encoding='utf-8', skip_blank_lines=True, error_bad_lines = False, skiprows = list(range(1,100000)),
-                   chunksize = 10000, verbose = True, na_filter=True, usecols = cols) #dtype=datatypes
+cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15]  #to skp index, source and meta_description column
+df_chunk = pd.read_csv(filename,
+                       encoding='utf-8',
+                       skip_blank_lines=True,
+                       error_bad_lines=False,
+                       skiprows=list(range(1, 100000)),
+                       chunksize=10000,
+                       verbose=True,
+                       na_filter=True,
+                       usecols=cols)  #dtype=datatypes
 
-#data = pd.read_csv(filename, encoding='utf-8', skip_blank_lines=True, verbose = True, na_filter=True)
-#print('Read data')
+# data = pd.read_csv(filename, encoding='utf-8', skip_blank_lines=True, verbose = True, na_filter=True)
+# print('Read data')
 
-#alternative way to load data. seem to give same result as pd.read_csv
+# alternative way to load data. seem to give same result as pd.read_csv
 """
 with open(filename, 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
@@ -118,120 +130,125 @@ print(rows[0])
 data = pd.DataFrame(rows)
 """
 
-chunk_no = 0 #there should be 100 chunks with 1mio dataset and 10000 line chunks
+chunk_no = 0  # there should be 100 chunks with 1mio dataset and 10000 line chunks
 chunk_list = []
 Tclean_start_time = time.time()
 for data in df_chunk:
-    
-    #data.dropna(subset = ['id'], inplace = True) #drops rows with NA ids and creates as a key error later on
-    #data["id"].map(type) != int #returns ids as indexes and bools from type() as values
+
+    # data.dropna(subset = ['id'], inplace = True) #drops rows with NA ids and creates as a key error later on
+    # data["id"].map(type) != int #returns ids as indexes and bools from type() as values
     data["id"].apply(pd.to_numeric, errors='coerce').dropna().astype(int)
-    
-    #to avoid NaNs and other nulls we set these to string ''
+
+    # to avoid NaNs and other nulls we set these to string ''
     data = data.where(pd.notnull(data), '')
-    
+
     clean_start_time = time.time()
-    #clean text
-    #we clean all at once with regex which results in a very long running time 
+    # clean text
+    # we clean all at once with regex which results in a very long running time
     for i in data['content'].index:
-        data.loc[i,'content'] = clean_text(data.loc[i,'content'])
+        data.loc[i, 'content'] = clean_text(data.loc[i, 'content'])
     chunk_list.append(data)
     chunk_no += 1
-    print('finished cleaning chunk {} ({} pct. of total) after {}s'.format(chunk_no, (chunk_no/100)*100, time.time()-clean_start_time))
+    print('finished cleaning chunk {} ({} pct. of total) after {}s'.format(
+        chunk_no, (chunk_no / 100) * 100,
+        time.time() - clean_start_time))
 
 data = pd.concat(chunk_list)
-print('finished cleaning after {}min'.format((time.time()-Tclean_start_time)/60))
+print('finished cleaning after {}min'.format(
+    (time.time() - Tclean_start_time) / 60))
 
-
-
-#define dicts
-author  = dict()
-domain  = dict()
-typ     = dict()
+# define dicts
+author = dict()
+domain = dict()
+typ = dict()
 keyword = dict()
 
-
-#we fill authors into author dictionary, sort to make sure they get the same id every time the code is run
+# we fill authors into author dictionary, sort to make sure they get the same id every time the code is run
 authors = []
 for i in data['authors']:
     split_authors = i.split(", ")
     for name in split_authors:
-        authors.append(name[:64-1]) #names must not be longer than 64 char
+        authors.append(name[:64 - 1])  #names must not be longer than 64 char
 
 print('finished splitting authors and making authors list')
 
 keywords = []
 keywords.append('')
 for words in data['meta_keywords']:
-    #splitter = words[1]
+    # splitter = words[1]
     words = words[2:-2]
     if words != '':
-        #split er ændret, da det split der var før lavede fejl ved dobbelt quotation 
+        # split er ændret, da det split der var før lavede fejl ved dobbelt quotation
         split_keywords = re.split("(?:\'|\"), (?:\'|\")", words)
         for word in split_keywords:
             if word != '':
-                keywords.append(word[:128-1].replace('\"', '\"\"')) #make sure that every keyword is no longer than 128 char
+                keywords.append(
+                    word[:128 - 1].replace('\"', '\"\"')
+                )  # make sure that every keyword is no longer than 128 char
 
 print('finished splitting keywords and making keywords list')
 
-
-#make dicts with key string and id int from sorted list
+# make dicts with key string and id int from sorted list
 putinDic(author, authors)
 
-putinDic(keyword,keywords)
+putinDic(keyword, keywords)
 
-putinDic(domain,data['domain'])
-domain.update({'NULL' : -1}) #add NULL entry in dict for misses
+putinDic(domain, data['domain'])
+domain.update({'NULL': -1})  # add NULL entry in dict for misses
 
-putinDic(typ,data['type'])
+putinDic(typ, data['type'])
 print('finished making dictionaries')
 
-
-#use create csv files for simple entities, ids created with dicts
+# use create csv files for simple entities, ids created with dicts
 simpleEntityToCSV("author_entity.csv", author)
 simpleEntityToCSV("keyword_entity.csv", keyword)
 simpleEntityToCSV("domain_entity.csv", domain)
 simpleEntityToCSV("type_entity.csv", typ)
-print('finished author_entity, keyword_entity, domain_entity, type_entity csv files')
+print(
+    'finished author_entity, keyword_entity, domain_entity, type_entity csv files'
+)
 
-
-#we need to make two new cols to get the correct ID of type and domain
-#will impact running time
+# we need to make two new cols to get the correct ID of type and domain
+# will impact running time
 domain_id = []
 type_id = []
 for i in data['domain']:
-    domain_id.append(int(domain.get(i,-1)))
+    domain_id.append(int(domain.get(i, -1)))
 for j in data['type']:
-    type_id.append(int(typ.get(j,-1)))
+    type_id.append(int(typ.get(j, -1)))
 
+# using first column as article id
+# is wrong due to false in
+# data.iloc[:,0].is_unique
+# true in
+# data['id'].is_unique
+# which we use later on
+# we do not need indexes and header when making csv
+article_entity = pd.concat([
+    data['id'], data['title'].str.lower(), data['content'],
+    data['summary'].str.lower(), data['meta_description'].str.lower(),
+    pd.DataFrame(type_id, columns=['type_id']), data['inserted_at'],
+    data['updated_at'], data['scraped_at']
+],
+                           axis=1)
+article_entity.to_csv('article_entity.csv',
+                      index=False,
+                      header=False,
+                      sep="^",
+                      encoding='utf-8')
 
-#using first column as article id
-#is wrong due to false in
-#data.iloc[:,0].is_unique
-#true in
-#data['id'].is_unique
-#which we use later on
-#we do not need indexes and header when making csv
-article_entity = pd.concat([data['id'],
-                                data['title'].str.lower(),
-                                data['content'],
-                                data['summary'].str.lower(),
-                                data['meta_description'].str.lower(),
-                                pd.DataFrame(type_id,columns=['type_id']),
-                                data['inserted_at'],
-                                data['updated_at'],
-                                data['scraped_at']],axis=1)
-article_entity.to_csv('article_entity.csv', index = False, header = False, sep = "^", encoding='utf-8')
+domain_id_df = pd.DataFrame(domain_id, columns=['domain_id'])
 
-domain_id_df = pd.DataFrame(domain_id, columns = ['domain_id'])
+webpage_relation = pd.concat([data['url'], data['id'], domain_id_df], axis=1)
 
-webpage_relation = pd.concat([data['url'],data['id'], domain_id_df], axis = 1)
-
-webpage_relation.to_csv('webpage_relation.csv',index = False, header = False, encoding='utf-8')
+webpage_relation.to_csv('webpage_relation.csv',
+                        index=False,
+                        header=False,
+                        encoding='utf-8')
 print('finished making article_entity webpage_relation csv files')
 
-#Pandas becomes difficult when working with undefined sizes so
-#we use file method for authors and keywords
+# Pandas becomes difficult when working with undefined sizes so
+# we use file method for authors and keywords
 
 tagsFile = open("tags_relation.csv", "w+", encoding="utf-8")
 article_id1 = 0
@@ -243,13 +260,14 @@ for m in data['meta_keywords']:
         for meta_keyword in split_keywords:
             meta_keyword = meta_keyword[:127].replace('\"', '\"\"')
             if meta_keyword != '':
-                tagsFile.write("%s,%s\n" % (data.loc[article_id1,'id'], keyword.get(meta_keyword.lower(),'')))
+                tagsFile.write("%s,%s\n" %
+                               (data.loc[article_id1, 'id'],
+                                keyword.get(meta_keyword.lower(), '')))
     article_id1 += 1
 
 tagsFile.close()
 
 print('finished making article_entity, webpage_relation csv files')
-
 
 writtenByFile = open("writtenBy_relation.csv", "w+", encoding="utf-8")
 article_id2 = 0
@@ -257,10 +275,13 @@ article_id2 = 0
 for k in data['authors']:
     split_authors = k.split(', ')
     for a in split_authors:
-        writtenByFile.write("%s,%s\n" % (data.loc[article_id2,"id"], author.get(a.lower(),'')))
+        writtenByFile.write(
+            "%s,%s\n" %
+            (data.loc[article_id2, "id"], author.get(a.lower(), '')))
     article_id2 += 1
 
 writtenByFile.close()
 print('finished making writtenBy_relation csv files')
 
-print("--- Total running time {} minutes ---".format((time.time() - start_time)/60))
+print("--- Total running time {} minutes ---".format(
+    (time.time() - start_time) / 60))
